789e4864cce785e06d9a47920d94ecca2116129f
VAI <kaneong5@gmail.com>
2026-02-27 19:00:23 +0700

    fix: move AI keys to serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
     images: {
         domains: ['imgs.deltafood.me', 'deltafoodpicture.sgp1.digitaloceanspaces.com']
     },
-    outputFileTracingRoot: path.join(__dirname)
+    outputFileTracingRoot: path.join(__dirname),
+    serverRuntimeConfig: {
+        COMET_API_KEY: process.env.COMET_API_KEY,
+        COMET_API_URL: process.env.COMET_API_URL,
+        COMET_MODEL: process.env.COMET_MODEL,
+    },
 }
 
 module.exports = nextConfig
